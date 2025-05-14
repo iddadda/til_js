9193a03fdd797a38070a2d4d58a2ea046ad4c822
iddadda <iddadda241@gmail.com>
2025-05-14 13:21:18 +0900

    [docs]: 생성자 함수, 클래스, 접근제한자, 상속, 콜백함수 사용처

--- js/lib.js
+++ js/lib.js
@@ -1,6 +1,12 @@
-function Coffe() {
-  console.log(this);
+const say = function () {};
+const cry = () => {};
+function smile() {}
+
+// 매개변수로 전달된 함수 실행
+function run(a) {
+  a();
 }
 
-Coffe();
-new Coffe();
+run(say);
+run(cry);
+run(smile); // smile은 변수가 아니라서 Error
