61eb77513bf546cf4fee2638eb8f002fd3141327
iddadda <iddadda241@gmail.com>
2025-05-15 13:22:41 +0900

    [docs]: 배열 공부 시작

--- js/lib.js
+++ js/lib.js
@@ -1,12 +1,1 @@
-const say = function () {};
-const cry = () => {};
-function smile() {}
-
-// 매개변수로 전달된 함수 실행
-function run(a) {
-  a();
-}
-
-run(say);
-run(cry);
-run(smile); // smile은 변수가 아니라서 Error
+const 제품 = ["사과", "딸기", "참외"];
